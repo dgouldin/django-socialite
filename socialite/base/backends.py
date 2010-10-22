7b12c62881bb6bf2b4b8d837b595011e48e7a87b
David Gouldin <david@gould.in>
2010-10-22 16:45:03 -0500

    Ripping out the stub of a backend system. This will require more design.

--- socialite/base/backends.py
+++ socialite/base/backends.py
@@ -1,19 +1,2 @@
-class BaseSocialObject(object):
-    property_list = [
-        'first_name',
-        'last_name',
-    ]
-    properties = {}
-    
-    def __getattr__(self, attr):
-        if attr not in self.property_list:
-            raise AttributeError("%s is not a valid attribute" % attr)
-        return self.properties.get(attr)
-        # TODO: this is unpythonic, change this all
-
-class BaseSocialBackend(object):
-    def get_unique_id(self):
-        raise NotImplementedError()
-
-    def find_friends(self, attr):
-        
+# TODO: Abstraction layer over various social networks,
+# registration system for adding additional services.
